88f3f339778c31089cf88437fb0f1494d6689276
xiaorongtao <xiaorongtao@autohome.com.cn>
2019-01-17 08:38:08 +0800

    [redis]
    Signed-off-by: xiaorongtao <xiaorongtao@autohome.com.cn>

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,14 +4,14 @@ require 'redis/objects'
 
 redis_config = Rails.application.config_for(:redis)
 
-$redis = Redis.new(host: redis_config['host'], port: redis_config['port'])
+$redis = Redis.new(host: redis_config['host'], port: redis_config['port'], password: redis_config['password'])
 $redis.select(0)
 Redis::Objects.redis = $redis
 
 sidekiq_url = "redis://#{redis_config['host']}:#{redis_config['port']}/0"
 Sidekiq.configure_server do |config|
-  config.redis = { namespace: 'sidekiq', url: sidekiq_url }
+  config.redis = { namespace: 'sidekiq', url: sidekiq_url, password: redis_config['password'] }
 end
 Sidekiq.configure_client do |config|
-  config.redis = { namespace: 'sidekiq', url: sidekiq_url }
+  config.redis = { namespace: 'sidekiq', url: sidekiq_url, password: redis_config['password'] }
 end
